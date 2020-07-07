20a54d045c3f3bb75fe4eb22a175983458e988ad
cnorris <charlotte.norris@metoffice.gov.uk>
2020-07-07 10:39:49 +0100

    Added basic integration test class

---
+++ CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
@@ -0,0 +1,10 @@
+package uk.gov.meto.netcdf.integration.test;
+
+public class IT_CDLConversion {
+
+	public static void main(String[] args) {
+		System.out.println("integration test running");
+
+	}
+
+}
