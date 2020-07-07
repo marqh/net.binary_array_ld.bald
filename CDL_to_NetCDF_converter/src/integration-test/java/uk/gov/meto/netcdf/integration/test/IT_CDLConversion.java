4e721f4d627201b531d2826fe77fae7feaaf45cb
cnorris <charlotte.norris@metoffice.gov.uk>
2020-07-07 14:05:53 +0100

    Logger added

--- CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
+++ CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
@@ -1,9 +1,14 @@
 package uk.gov.meto.netcdf.integration.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class IT_CDLConversion {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(IT_CDLConversion.class);
 
 	public static void main(String[] args) {
-		System.out.println("integration test running");
+		LOG.debug("integration test running");
 
 	}
 
