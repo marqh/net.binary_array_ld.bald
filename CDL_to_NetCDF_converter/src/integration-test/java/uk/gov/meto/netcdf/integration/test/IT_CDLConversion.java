7af7a13e303c0e4e1bde148ff17b2fa0e8a79b63
cnorris <charlotte.norris@metoffice.gov.uk>
2020-07-09 15:01:01 +0100

    Integration test added Reading of a NetCDF File

--- CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
+++ CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
@@ -3,13 +3,40 @@ package uk.gov.meto.netcdf.integration.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.gov.meto.netcdf.cdl.Application;
+import ucar.nc2.NetcdfFile;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
 public class IT_CDLConversion {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(IT_CDLConversion.class);
 
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		LOG.debug("integration test running");
 
+	}*/
+	@Test
+	public void shouldConvertToReadableNetCDF() {
+		Application app = new Application();
+		app.main(null);
+		  String filename = "C:/data/my/file.nc";
+		  NetcdfFile ncfile = null;
+		  try {
+		    ncfile = NetcdfFile.open(filename);
+		   // process( ncfile);
+		  } catch (IOException ioe) {
+		    LOG.error("trying to open " + filename, ioe);
+		  } finally { 
+		    if (null != ncfile) try {
+		      ncfile.close();
+		      LOG.debug(ncfile.toString());
+		    } catch (IOException ioe) {
+		      LOG.error("trying to close " + filename, ioe);
+		    }
+		  }
 	}
 
 }
