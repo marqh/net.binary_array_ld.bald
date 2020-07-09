ccc6209af2a39b4414f7dde8267f615f82823d1d
cnorris <charlotte.norris@metoffice.gov.uk>
2020-07-09 17:36:48 +0100

    Added cdl to netcdf conversion

--- CDL_to_NetCDF_converter/src/main/java/uk/gov/meto/netcdf/cdl/Application.java
+++ CDL_to_NetCDF_converter/src/main/java/uk/gov/meto/netcdf/cdl/Application.java
@@ -1,10 +1,24 @@
 package uk.gov.meto.netcdf.cdl;
 
+import java.io.IOException;
+
+import uk.gov.meto.netcdf.integration.test.Logger;
+
 public class Application {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		System.out.println("Starting the main method");
-		
+		String command = "ncgen -o " + "C:/data/my/file.nc" + " " + "array_geo.cdl";
+		Process process;
+		try {
+			process = Runtime.getRuntime()
+				      .exec(command);
+			int exitCode = process.waitFor();
+		} catch (IOException e) {
+			LOG.error("process creation failed");
+		}
 		
 
 	}
