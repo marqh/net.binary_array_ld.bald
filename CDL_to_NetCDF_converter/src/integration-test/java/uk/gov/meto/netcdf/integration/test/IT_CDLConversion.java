d282bf10ea94cc4d3e16b486917ca3de13a16657
cnorris <charlotte.norris@metoffice.gov.uk>
2020-07-10 10:22:28 +0100

    Assertion removed, logger reconfigured, formatting fixed

--- CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
+++ CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
@@ -1,7 +1,6 @@
 package uk.gov.meto.netcdf.integration.test;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import uk.gov.meto.netcdf.cdl.Application;
 import ucar.nc2.NetcdfFile;
@@ -14,37 +13,32 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
 public class IT_CDLConversion {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(IT_CDLConversion.class);
 
-/*	public static void main(String[] args) {
-		LOG.debug("integration test running");
+	private static final Logger LOG = Logger.getLogger(IT_CDLConversion.class);
 
-	}*/
 	@Test
 	public void shouldConvertToReadableNetCDF() {
 		Application app = new Application();
 		app.main(null);
-		  String filename = "C:/data/my/file.nc";
-		  NetcdfFile ncfile = null;
-		  try {
-			  assertNull(ncfile);
-		    ncfile = NetcdfFile.open(filename);
-		   // process( ncfile);
-		  } catch (IOException ioe) {
+		String filename = "C:/data/my/file.nc";
+		NetcdfFile ncfile = null;
+		try {
+			assertNull(ncfile);
+			ncfile = NetcdfFile.open(filename);
+			// process( ncfile);
+		} catch (IOException ioe) {
 			fail("Unexpected exception: " + ioe.getMessage());
-		    LOG.error("trying to open " + filename, ioe);
-		  } finally { 
-			assertNotNull(ncfile);
-		    if (null != ncfile) try {
-		      ncfile.close();
-		      System.out.println(ncfile.toString() );
-		      LOG.debug(ncfile.toString());
-		    } catch (IOException ioe) {
-		      fail("Unexpected exception: " + ioe.getMessage());
-		      LOG.error("trying to close " + filename, ioe);
-		    }
-		  }
+			LOG.error("trying to open " + filename, ioe);
+		} finally {
+			if (null != ncfile)
+				try {
+					ncfile.close();
+					System.out.println(ncfile.toString());
+					LOG.debug(ncfile.toString());
+				} catch (IOException ioe) {
+					fail("Unexpected exception: " + ioe.getMessage());
+					LOG.error("trying to close " + filename, ioe);
+				}
+		}
 	}
-
 }
--- CDL_to_NetCDF_converter/src/main/java/uk/gov/meto/netcdf/cdl/Application.java
+++ CDL_to_NetCDF_converter/src/main/java/uk/gov/meto/netcdf/cdl/Application.java
@@ -3,11 +3,10 @@ package uk.gov.meto.netcdf.cdl;
 import java.io.IOException;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Application {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+	private static final Logger LOG = Logger.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		System.out.println("Starting the main method");
@@ -15,13 +14,11 @@ public class Application {
 		LOG.debug("Command is: {}", command);
 		Process process;
 		try {
-			process = Runtime.getRuntime()
-				      .exec(command);
+			process = Runtime.getRuntime().exec(command);
 			int exitCode = process.waitFor();
 		} catch (Exception e) {
 			LOG.error("process creation failed");
 		}
-		
 
 	}
 
