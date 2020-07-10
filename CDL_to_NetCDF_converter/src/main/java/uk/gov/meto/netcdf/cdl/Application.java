67f6b0ea38ece7f4f31a50cec642efda0a3e95bf
cnorris <charlotte.norris@metoffice.gov.uk>
2020-07-10 14:00:20 +0100

    Change to process builder

--- CDL_to_NetCDF_converter/src/main/java/uk/gov/meto/netcdf/cdl/Application.java
+++ CDL_to_NetCDF_converter/src/main/java/uk/gov/meto/netcdf/cdl/Application.java
@@ -3,6 +3,9 @@ package uk.gov.meto.netcdf.cdl;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,16 +17,19 @@ public class Application {
 	public static void main(String[] args) {
 		URL url = Application.class.getClass().getResource("/array_geo.cdl");
 		System.out.println("Starting the main method: " + url.getPath());
-		String command = "ncgen -o " + "array_geo.nc" + " " + url.getPath();
-		LOG.debug("Command is: {}", command);
+		List<String> callParams = new ArrayList<String>(
+		        Arrays.asList("ncgen", "-o", "array_geo.nc", url.getPath()));
+		LOG.debug("Command is: {}", callParams);
 		Process process;
 		try {
 			System.out.println(new File(url.getPath()).exists());
-			process = Runtime.getRuntime().exec(command);
+			ProcessBuilder processBuilder = new ProcessBuilder(callParams);
+			process = processBuilder.start();
 			int exitCode = process.waitFor();
 			System.out.println(new File("array_geo.nc").exists());
 		} catch (Exception e) {
 			LOG.error("process creation failed");
+			throw e;
 		}
 
 	}
