0c2cd1039663f02e4bdcee3dbc115b170b11ad78
Simon Oakes <simon.oakes@epimorphics.com>
2020-10-23 18:59:49 +0100

    ADD Conversion API

---
+++ binary-array-ld-lib/src/main/kotlin/net/bald/model/ModelBinaryArrayConverter.kt
@@ -0,0 +1,19 @@
+package net.bald.model
+
+import net.bald.BinaryArray
+import org.apache.jena.rdf.model.Model
+import org.apache.jena.rdf.model.ModelFactory
+
+/**
+ * API for converting a [BinaryArray] to a linked data [Model].
+ */
+object ModelBinaryArrayConverter {
+    /**
+     * Convert the given binary array metadata into a linked data model.
+     * @param ba The binary array to convert.
+     * @return The resulting model.
+     */
+    fun convert(ba: BinaryArray): Model {
+        TODO()
+    }
+}
