b7e1a2b76064b05189066c8522f46f8d2bb92a6d
Simon Oakes <simon.oakes@epimorphics.com>
2020-10-23 19:03:11 +0100

    Initial CLI stub

---
+++ binary-array-ld-cli/src/main/kotlin/net/bald/BinaryArrayConvertCli.kt
@@ -0,0 +1,14 @@
+package net.bald
+
+/**
+ * Command Line Interface for converting NetCDF metadata to Linked Data graphs.
+ */
+class BinaryArrayConvertCli {
+    fun run(vararg args: String) {
+        TODO()
+    }
+}
+
+fun main(args: Array<String>) {
+    BinaryArrayConvertCli().run(*args)
+}
