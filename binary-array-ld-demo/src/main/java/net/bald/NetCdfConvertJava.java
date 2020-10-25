5dfb1eda03c118b3bfe118a7f84c28560c0e3084
Simon Oakes <simon.oakes@epimorphics.com>
2020-10-25 13:58:03 +0000

    Add demo module (Java)

---
+++ binary-array-ld-demo/src/main/java/net/bald/NetCdfConvertJava.java
@@ -0,0 +1,26 @@
+package net.bald;
+
+import net.bald.model.ModelBinaryArrayConverter;
+import net.bald.netcdf.NetCdfBinaryArray;
+import org.apache.jena.rdf.model.Model;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+/**
+ * Demonstration of how to call the API in Java code.
+ */
+public class NetCdfConvertJava {
+    public static void convert(String inputLoc, String outputLoc, String format) throws Exception {
+        BinaryArray ba = NetCdfBinaryArray.create(inputLoc, "http://test.binary-array-ld.net/example");
+        Model model = ModelBinaryArrayConverter.convert(ba);
+
+        try (OutputStream output = new FileOutputStream(outputLoc)) {
+            model.write(output, format);
+        }
+    }
+
+    public static void convert() throws Exception {
+        convert("/path/to/input.nc", "/path/to/input.ttl", "ttl");
+    }
+}
